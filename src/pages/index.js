95c004db3f466e5fd4ba469522977a2bde239ebf
christianmbonet <christianm.bonet@gmail.com>
2021-04-28 21:35:10 -0400

    adding squares, colors and arranging them

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
-import React from "react"
+import React from "react";
+import './../components/index.css'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div className='container'>
+    <div className='topRow'>
+    <div className='firstBlock'>
+      <div className='firstPink'></div>
+      <div className='firstBlue'></div>
+    </div>
+    <div className='secondBlock'>
+      <div className='secondBlue'></div>
+      <div className='secondPink'></div>
+    </div>
+    </div>
+  </div>
 }
