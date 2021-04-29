43fd17fc3622d07bb9182f76a91577e99e55f0a5
christianmbonet <christianm.bonet@gmail.com>
2021-04-29 09:22:48 -0400

    changed pink color and edited grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,22 @@ import './../components/index.css'
 export default function Home() {
   return <div className='container'>
     <div className='topRow'>
-    <div className='firstBlock'>
-      <div className='firstPink'></div>
-      <div className='firstBlue'></div>
+      <div className='topLeft'>
+        <div className='firstPink'></div>
+        <div className='firstBlue'></div>
+      </div>
+      <div className='topRight'>
+        <div className='secondBlue'></div>
+        <div className='secondPink'></div>
+      </div>
     </div>
-    <div className='secondBlock'>
-      <div className='secondBlue'></div>
-      <div className='secondPink'></div>
-    </div>
+    <div className='bottomRow'>
+        <div className='bottomLeft'></div>
+          <div className='thirdPink'></div>
+          <div className='thirdBlue'></div>
+        <div className='bottomRight'></div>
+          <div className='fourthBlue'></div>
+          <div className='fourthPink'></div>
     </div>
   </div>
 }
