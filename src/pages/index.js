aab616cca0997ff134be6b881c3d839bda645943
christianmbonet <christianm.bonet@gmail.com>
2021-04-29 20:49:47 -0400

    added more tile color changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,23 +5,23 @@ export default function Home() {
    
   const [on, setOn] = useState(false);
 
-  const lightSwitch = () => setOn(on => !on);
+  const lightSwitchCenter = () => setOn(on => !on);
 
   return <div>
   <div className='container'>
     <div className='topRow'>
         <div className='topLeftPink'></div>
-        <div className='topMiddleBlue'></div>
+        <div className={on ? 'topMiddleBlue' : 'topMiddlePink'}></div>
         <div className='topRightPink'></div>
     </div>
     <div className='middleRow'>
-        <div className='middleLeftBlue'></div>
-        <button className='middleMiddle' onClick={lightSwitch}></button>
+        <div className={on ? 'middleLeftBlue': 'middleLeftPink'}></div>
+        <button className='middleMiddle' onClick={lightSwitchCenter}></button>
         <div className={on ? 'middleRightBlue' : 'middleRightPink'}></div>
     </div>
     <div className='bottomRow'>
         <div className='bottomLeftPink'></div>
-        <div className='bottomMiddleBlue'></div>
+        <div className={on ? 'bottomMiddleBlue' : 'bottomMiddlePink'}></div>
         <div className='bottomRightPink'></div>
     </div>
   </div>
